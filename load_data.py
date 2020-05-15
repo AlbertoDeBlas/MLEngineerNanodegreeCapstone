64f69f9ff3274e91b70c84670ca86b7ae693fdb9
Alberto de Blas <alberto.de.blas@gmail.com>
2020-05-15 09:13:57 +0000

    Move load_dataframe_from_s3 to load_data.py
    refactor: Move load_dataframe_from_s3 to load_data.py

--- load_data.py
+++ load_data.py
@@ -1,4 +1,6 @@
-
+import re
+import io
+import pandas as pd
 
 def filter_csv(string):
     return re.search(r'.csv', string)
@@ -10,7 +12,7 @@ def list_csv_files(obj_list):
 		obj_list (dict): list of objects
 		
 	Returns:
-		filterd_list (list): list of csv files
+		filtered_list (list): list of csv files
 	'''
     files=[]
     for contents in obj_list['Contents']:
@@ -19,4 +21,21 @@ def list_csv_files(obj_list):
     filtered_list = list(filter(filter_csv, files))
 
     return filtered_list
+
+def load_dataframe_from_s3(s3_client, bucket, name):
+    '''Returns a csv file from s3
     
+    Args:
+        s3_client(): s3 client object
+        bucket: s3 bucket where the file is located
+        name: name of the csv file
+        
+    Returns:
+        pandas dataframe
+    
+    '''
+    data_object = s3_client.get_object(Bucket=bucket, Key=name)
+    data_body = data_object["Body"].read()
+    data_stream = io.BytesIO(data_body)
+    
+    return pd.read_csv(data_stream, header=0, delimiter=",") 
